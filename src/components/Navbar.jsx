09a329ecc16693c56ef7140374061ebeb7746b73
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-05-01 11:53:19 -0700

    Create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import React from "react";
+
+export default function Navbar() {
+  const navItems = ["Home", "About", "Projects", "Contact"];
+
+  return (
+    <AppBar component="nav">
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Typography
+          variant="h6"
+          sx={{ flexGrow: 0, display: { xs: "none", sm: "block" } }}
+        >
+          Juliana | Full Stack Developer
+        </Typography>
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          {navItems.map((item) => (
+            <Button key={item} sx={{ color: "white" }}>
+              {item}
+            </Button>
+          ))}
+          Resume
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
