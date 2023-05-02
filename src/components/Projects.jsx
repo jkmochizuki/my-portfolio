0cf3cb17bda6652349568abab663463d735bd9da
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-05-01 19:38:02 -0700

    Create constants file

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,39 +6,15 @@ import {
   Typography,
   CardHeader,
 } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, A11y } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "swiper/css/scrollbar";
+import { projects } from "../constants";
 
 export default function Projects() {
-  const projects = [
-    {
-      name: "Codi",
-      image: "/images/codi-project.png",
-      about:
-        "A coding-oriented flashcards app to help developers and students retain languages and frameworks concepts effectively.",
-      stack:
-        "React, Axios, Nodejs, Express.js , Prisma ORM, PostgreSQL, Auth0, OpenAPI, Material UI.",
-    },
-    {
-      name: "Scheduler",
-      image: "/images/scheduler-project.png",
-      about:
-        "React scheduler application that allows users to book and cancel interviews.",
-      stack: "React, Axios, Classnames, Normalize, Storybook.",
-    },
-    {
-      name: "Tweeter",
-      image: "/images/tweeter-project.png",
-      about: "Tweeter is a simple, single-page Twitter clone.",
-      stack: "Express, jQuery, PostgreSQL, Node.",
-    },
-  ];
-
   const ProjectSlides = () => {
     return (
       <Swiper
@@ -56,8 +32,7 @@ export default function Projects() {
                 component="img"
                 height="450"
                 image={process.env.PUBLIC_URL + p.image}
-              >
-              </CardMedia>
+              ></CardMedia>
               <CardContent sx={{ mx: "auto" }}>
                 <Typography variant="body2" color="text.secondary">
                   {p.about}
@@ -76,7 +51,7 @@ export default function Projects() {
   return (
     <Grid container sx={{ height: "100vh" }} columns={{ xs: 12 }}>
       <Grid item xs={12}>
-        <Typography variant="h3">Projects</Typography>
+        <Typography variant="h4">Projects</Typography>
       </Grid>
       <Grid item xs={12}>
         <ProjectSlides />
---
+++ src/constants/index.js
@@ -0,0 +1,42 @@
+export const skills = [
+  { name: "JavaScript", class: "devicon-javascript-plain colored" },
+  { name: "Ruby", class: "devicon-ruby-plain colored" },
+  { name: "HTML", class: "devicon-html5-plain" },
+  { name: "CSS", class: "devicon-css3-plain colored" },
+  { name: "React JS", class: "devicon-react-original colored" },
+  { name: "Node JS", class: "devicon-nodejs-plain colored" },
+  { name: "Rails", class: "devicon-rails-plain-wordmark colored" },
+  { name: "Socket.io", class: "devicon-socketio-original colored" },
+  { name: "Material UI", class: "devicon-materialui-plain colored" },
+  { name: "jQuery", class: "devicon-jquery-plain colored" },
+  { name: "SASS", class: "devicon-sass-original colored" },
+  { name: "Bootstrap", class: "devicon-bootstrap-plain colored" },
+  { name: "PostgreSQL", class: "devicon-postgresql-plain colored" },
+  { name: "Mocha", class: "devicon-mocha-plain colored" },
+  { name: "Jest", class: "devicon-jest-plain colored" },
+  { name: "Storybook", class: "devicon-storybook-plain colore" },
+];
+
+export const projects = [
+  {
+    name: "Codi",
+    image: "/images/codi-project.png",
+    about:
+      "A coding-oriented flashcards app to help developers and students retain languages and frameworks concepts effectively.",
+    stack:
+      "React, Axios, Nodejs, Express.js , Prisma ORM, PostgreSQL, Auth0, OpenAPI, Material UI.",
+  },
+  {
+    name: "Scheduler",
+    image: "/images/scheduler-project.png",
+    about:
+      "React scheduler application that allows users to book and cancel interviews.",
+    stack: "React, Axios, Classnames, Normalize, Storybook.",
+  },
+  {
+    name: "Tweeter",
+    image: "/images/tweeter-project.png",
+    about: "Tweeter is a simple, single-page Twitter clone.",
+    stack: "Express, jQuery, PostgreSQL, Node.",
+  },
+];
