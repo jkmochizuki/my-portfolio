ac1f6ba7620066b5d4d5c30e5383e2655618bd4d
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-05-01 22:06:29 -0700

    Add style to hovered projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,33 @@
 import {
   Grid,
   Card,
-  CardContent,
   CardMedia,
   Typography,
   CardHeader,
+  CardActionArea,
+  Box,
+  Link,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, A11y } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { projects } from "../constants";
+import { projectStyles } from "../styles/projectStyles";
 
 export default function Projects() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovered(false);
+  };
+
   const ProjectSlides = () => {
     return (
       <Swiper
@@ -27,20 +40,39 @@ export default function Projects() {
         {projects.map((p) => (
           <SwiperSlide>
             <Card sx={{ maxWidth: 800, mx: "auto", border: 1 }}>
-              <CardHeader title={p.name} />
-              <CardMedia
-                component="img"
-                height="450"
-                image={process.env.PUBLIC_URL + p.image}
-              ></CardMedia>
-              <CardContent sx={{ mx: "auto" }}>
-                <Typography variant="body2" color="text.secondary">
-                  {p.about}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Tech Stack: {p.stack}
-                </Typography>
-              </CardContent>
+              <CardActionArea
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+              >
+                <CardHeader title={p.name} />
+                {!isHovered ? (
+                  <CardMedia
+                    component="img"
+                    height="450"
+                    image={process.env.PUBLIC_URL + p.image}
+                  />
+                ) : (
+                  <>
+                    <CardMedia
+                      component="img"
+                      height="450"
+                      image={process.env.PUBLIC_URL + p.image}
+                      style={projectStyles.media}
+                    />
+                    <Box style={projectStyles.overlay}>
+                      <Typography variant="h6" color="text.primary">
+                        <Link href="#">Live</Link> | <Link href="#">Repo</Link>
+                      </Typography>
+                      <Typography variant="h6" color="text.secondary">
+                        {p.about}
+                      </Typography>
+                      <Typography variant="h6" color="text.secondary">
+                        Tech Stack: {p.stack}
+                      </Typography>
+                    </Box>
+                  </>
+                )}
+              </CardActionArea>
             </Card>
           </SwiperSlide>
         ))}
@@ -49,12 +81,14 @@ export default function Projects() {
   };
 
   return (
-    <Grid container sx={{ height: "100vh" }} columns={{ xs: 12 }}>
-      <Grid item xs={12}>
-        <Typography variant="h4">Projects</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <ProjectSlides />
+    <Grid container sx={{ height: "100vh" }}>
+      <Grid xs={12} container p={5} marginBottom={5} sx={{ height: "95vh" }}>
+        <Grid item xs={12}>
+          <Typography variant="h4">Projects</Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <ProjectSlides />
+        </Grid>
       </Grid>
     </Grid>
   );
---
+++ src/styles/projectStyles.js
@@ -0,0 +1,15 @@
+export const projectStyles = {
+  media: {
+    opacity: 0.1,
+    backgroundColor: "rgba(76, 175, 80, 0.3)"
+  },
+
+  overlay: {
+    position: "absolute",
+    top: "200px",
+    backgroundColor: "rgba(76, 175, 80, 0.3)"
+  },
+
+};
+
+
