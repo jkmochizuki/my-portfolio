4cbc9f6015c22acfaf1fc6c80448e20e9915ee5d
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-05-04 22:16:29 -0700

    Update skills

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,22 +1,34 @@
-export const navItems = ["About", "My Skills", "Projects", "Contact"];
-
 export const skills = [
   { name: "JavaScript", class: "devicon-javascript-plain colored" },
   { name: "Ruby", class: "devicon-ruby-plain colored" },
-  { name: "HTML", class: "devicon-html5-plain" },
-  { name: "CSS", class: "devicon-css3-plain colored" },
-  { name: "React JS", class: "devicon-react-original colored" },
-  { name: "Node JS", class: "devicon-nodejs-plain colored" },
+  { name: "Node.js", class: "devicon-nodejs-plain colored" },
+  {
+    name: "Express.js",
+    src: "https://img.icons8.com/?size=512&id=WNoJgbzDr3i2&format=png",
+  },
   { name: "Rails", class: "devicon-rails-plain-wordmark colored" },
-  { name: "Socket.io", class: "devicon-socketio-original colored" },
-  { name: "Material UI", class: "devicon-materialui-plain colored" },
+  { name: "HTML", class: "devicon-html5-plain colored" },
+  { name: "CSS", class: "devicon-css3-plain colored" },
   { name: "jQuery", class: "devicon-jquery-plain colored" },
+  { name: "React", class: "devicon-react-original colored" },
+  { name: "MUI", class: "devicon-materialui-plain colored" },
   { name: "SASS", class: "devicon-sass-original colored" },
   { name: "Bootstrap", class: "devicon-bootstrap-plain colored" },
+  {
+    name: "SQL",
+    src: "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_sql_icon_130152.png",
+  },
   { name: "PostgreSQL", class: "devicon-postgresql-plain colored" },
+  {
+    name: "Prisma",
+    src: "https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_light_prisma_icon_130444.png",
+  },
+
   { name: "Mocha", class: "devicon-mocha-plain colored" },
   { name: "Jest", class: "devicon-jest-plain colored" },
-  { name: "Storybook", class: "devicon-storybook-plain colore" },
+  { name: "Storybook", class: "devicon-storybook-plain colored" },
+
+  { name: "Socket.IO", class: "devicon-socketio-original" },
 ];
 
 export const projects = [
@@ -26,9 +38,9 @@ export const projects = [
     about:
       "A coding-oriented flashcards app to help developers and students retain languages and frameworks concepts effectively.",
     stack:
-      "React, Axios, Nodejs, Express.js , Prisma ORM, PostgreSQL, Auth0, OpenAPI, Material UI.",
-      live: "https://codi-app.netlify.app",
-      repo: "https://github.com/PavelKotlov/codi-client"
+      "React, Axios, Nodejs, Express.js, Prisma ORM, PostgreSQL, Auth0, OpenAPI, Material UI.",
+    live: "https://codi-app.netlify.app",
+    repo: "https://github.com/PavelKotlov/codi-client",
   },
   {
     name: "Scheduler",
@@ -36,15 +48,25 @@ export const projects = [
     about:
       "React scheduler application that allows users to book and cancel interviews.",
     stack: "React, Axios, Classnames, Normalize, Storybook.",
-    live: "",
-    repo: "https://github.com/jkmochizuki/scheduler"
+    live: "https://interviewscheduler-app.netlify.app/",
+    repo: "https://github.com/jkmochizuki/scheduler",
   },
+  {
+    name: "TinyApp",
+    image: "/images/tinyapp-project.png",
+    about:
+      "TinyApp is a full stack web application that allows users to shorten long URLs.",
+    stack: "Node, Express",
+    live: "https://tinyapp.herokuapp.com/",
+    repo: "https://github.com/jkmochizuki/tinyapp",
+  },
+
   {
     name: "Tweeter",
     image: "/images/tweeter-project.png",
     about: "Tweeter is a simple, single-page Twitter clone.",
     stack: "Express, jQuery, PostgreSQL, Node.",
     live: "",
-    repo: "https://github.com/jkmochizuki/tweeter"
+    repo: "https://github.com/jkmochizuki/tweeter",
   },
 ];
