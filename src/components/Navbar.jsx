3afc7c7e2b70b39fccd13f7543264290c0afea16
Juliana Mochizuki <jkmochizuki@gmail.com>
2023-05-10 10:55:34 -0700

    Udpate navbar to display on scroll up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   AppBar,
   Box,
@@ -9,7 +9,6 @@ import {
   Stack,
   ThemeProvider,
   Typography,
-  useMediaQuery,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import HorizontalRuleIcon from "@mui/icons-material/HorizontalRule";
@@ -26,20 +25,14 @@ import { useInView } from "react-intersection-observer";
 
 export default function Navbar(props) {
   const [isOpen, setIsOpen] = useState(false);
-  const [isHome, setIsHome] = useState(true);
+  const [isScrollUp, setIsScrollUp] = useState(true);
+  const prevScrollPos = useRef(0);
   const [isLoading, setIsLoading] = useState(false);
-  const isMediumScreen = useMediaQuery((theme) => theme.breakpoints.down("md"));
 
   const { ref, inView } = useInView({
     triggerOnce: true,
     threshold: 0.25,
   });
-  // const { isctive, ...rest } = props;
-
-  // function Wrapper(props) {
-  //   const { isActive, ...rest } = props
-  //     return <div {...rest} />
-  // }
 
   const toggleDrawer = () => {
     setIsLoading(true);
@@ -51,12 +44,15 @@ export default function Navbar(props) {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.pageYOffset > 50) {
-        setIsHome(false);
+      const currentScrollPos = window.pageYOffset;
+      if (prevScrollPos.current > currentScrollPos) {
+        setIsScrollUp(true);
       } else {
-        setIsHome(true);
+        setIsScrollUp(false);
       }
+      prevScrollPos.current = currentScrollPos;
     };
+
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -73,105 +69,114 @@ export default function Navbar(props) {
 
   return (
     <ThemeProvider theme={theme}>
-      <AppBar
-        sx={navbarStyles.root}
-        position="fixed"
-        className={inView ? "section" : "opacity-0"}
-        ref={ref}
-      >
-        {/* navbar */}
-        <Box
-          sx={navbarStyles.navSection}
-          className={`container ${inView ? "slide-in-nav" : ""}`}
-        >
-          {isHome || isMediumScreen ? (
-            <Box
-              component="img"
-              src={process.env.PUBLIC_URL + "/images/logo.png"}
-              alt=""
-              sx={navbarStyles.navItems}
-            />
-          ) : (
-            <Box sx={navbarStyles.navItems} />
-          )}
-          <IconButton sx={navbarStyles.iconButton} onClick={toggleDrawer}>
-            {!isLoading && !isOpen && <MenuIcon sx={navbarStyles.menuIcon} />}
-            {isLoading && <HorizontalRuleIcon sx={navbarStyles.menuIcon} />}
-            {!isLoading && isOpen && <CloseIcon sx={navbarStyles.menuIcon} />}
-          </IconButton>
-        </Box>
-
-        {/* menu drawer */}
-        <Drawer
-          anchor="right"
-          open={isOpen}
-          onClose={toggleDrawer}
-          PaperProps={{
-            sx: navbarStyles.drawerPaperProps,
+      {isScrollUp && (
+        <AppBar
+          sx={{
+            ...navbarStyles.root,
+            backgroundColor:
+              isOpen || isLoading ? "transparent" : "rgba(23, 26, 38, 0.9)",
           }}
-          variant="modal"
-          transitionDuration={500}
+          position="fixed"
+          ref={ref}
         >
-          <Box sx={navbarStyles.drawerContainer}>
-            {/* menu options */}
-            <Box sx={navbarStyles.menuSection}>
-              {menuOptions.map((o) => (
-                <NavHashLink
-                  to={o.to}
-                  smooth
-                  style={navbarStyles.hashLink}
-                  onClick={handleClink}
-                  key={o.name}
-                >
-                  <Typography sx={navbarStyles.textOptions}>
-                    {o.name}
-                  </Typography>
-                </NavHashLink>
-              ))}
-              {/* icons */}
-              <Hidden mdUp>
-                <Grid item xs={12}>
-                  <Stack direction="row" spacing={1}>
-                    <LinkedInIcon
-                      sx={navbarStyles.icon}
-                      onClick={() =>
-                        window.open(
-                          "https://www.linkedin.com/in/jmochizuki/",
-                          "_blank"
-                        )
-                      }
-                    />
-                    <GitHubIcon
-                      sx={navbarStyles.icon}
-                      onClick={() =>
-                        window.open(
-                          "https://github.com/julianamochizuki",
-                          "_blank"
-                        )
-                      }
-                    />
-                    <EmailIcon
-                      sx={navbarStyles.icon}
-                      onClick={() =>
-                        window.open("mailto:jmochizuki.dev@gmail.com")
-                      }
-                    />
-                    <DescriptionIcon
-                      sx={navbarStyles.icon}
-                      onClick={() =>
-                        window.open(
-                          "https://resume.creddle.io/resume/3bkcgktacjr",
-                          "_blank"
-                        )
-                      }
-                    />
-                  </Stack>
-                </Grid>
-              </Hidden>
-            </Box>
+          {/* navbar */}
+          <Box
+            sx={navbarStyles.navSection}
+            className={`container ${inView ? "slide-in-nav" : ""}`}
+          >
+            {!isOpen ? (
+              <Box
+                component="img"
+                src={process.env.PUBLIC_URL + "/images/logo.png"}
+                alt=""
+                sx={navbarStyles.navItems}
+              />
+            ) : (
+              <Box sx={navbarStyles.navItems} />
+            )}
+
+            <IconButton
+              sx={navbarStyles.iconButton}
+              onClick={isOpen ? handleClink : toggleDrawer}
+            >
+              {!isLoading && !isOpen && <MenuIcon sx={navbarStyles.menuIcon} />}
+              {isLoading && <HorizontalRuleIcon sx={navbarStyles.menuIcon} />}
+              {!isLoading && isOpen && <CloseIcon sx={navbarStyles.menuIcon} />}
+            </IconButton>
           </Box>
-        </Drawer>
-      </AppBar>
+
+          {/* menu drawer */}
+          <Drawer
+            anchor="right"
+            open={isOpen}
+            onClose={toggleDrawer}
+            PaperProps={{
+              sx: navbarStyles.drawerPaperProps,
+            }}
+            variant="temporary"
+            transitionDuration={500}
+          >
+            <Box sx={navbarStyles.drawerContainer}>
+              {/* menu options */}
+              <Box sx={navbarStyles.menuSection}>
+                {menuOptions.map((o) => (
+                  <NavHashLink
+                    to={o.to}
+                    smooth
+                    style={navbarStyles.hashLink}
+                    onClick={handleClink}
+                    key={o.name}
+                  >
+                    <Typography sx={navbarStyles.textOptions}>
+                      {o.name}
+                    </Typography>
+                  </NavHashLink>
+                ))}
+                {/* icons */}
+                <Hidden mdUp>
+                  <Grid item xs={12}>
+                    <Stack direction="row" spacing={1}>
+                      <LinkedInIcon
+                        sx={navbarStyles.icon}
+                        onClick={() =>
+                          window.open(
+                            "https://www.linkedin.com/in/jmochizuki/",
+                            "_blank"
+                          )
+                        }
+                      />
+                      <GitHubIcon
+                        sx={navbarStyles.icon}
+                        onClick={() =>
+                          window.open(
+                            "https://github.com/julianamochizuki",
+                            "_blank"
+                          )
+                        }
+                      />
+                      <EmailIcon
+                        sx={navbarStyles.icon}
+                        onClick={() =>
+                          window.open("mailto:jmochizuki.dev@gmail.com")
+                        }
+                      />
+                      <DescriptionIcon
+                        sx={navbarStyles.icon}
+                        onClick={() =>
+                          window.open(
+                            "https://resume.creddle.io/resume/3bkcgktacjr",
+                            "_blank"
+                          )
+                        }
+                      />
+                    </Stack>
+                  </Grid>
+                </Hidden>
+              </Box>
+            </Box>
+          </Drawer>
+        </AppBar>
+      )}
     </ThemeProvider>
   );
 }
